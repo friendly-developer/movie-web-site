b473a0df0d4ddc49f96e0cf7e269915f5614bfee
gvsvarun <gvsvarun@gmail.com>
2017-02-05 14:07:21 +0530

    initial commit

---
+++ entertainment_center.py
@@ -0,0 +1,25 @@
+# Lesson 3.4: Make Classes
+# Mini-Project: Movies Website
+
+# In this file, you will define instances of the class Movie defined
+# in media.py. After you follow along with Kunal, make some instances
+# of your own!
+
+# After you run this code, open the file fresh_tomatoes.html to
+# see your webpage!
+
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie('Toy Story',
+	'Story about how toys of a kid comes to life',
+	'https://lh5.ggpht.com/ZcvtEd-gylgBYg-HFuzo_YNCKnbyL1aOsY7XKqEf0FiULcLBoWaNx9nRN1ymfcy9vkRj=w300',
+	'https://www.youtube.com/watch?v=KYz2wyBy3kc','')
+
+avatar = media.Movie('Avatar',
+	'Marine landing on an Alien Planet',
+	'http://imgs.abduzeedo.com/files/articles/Avatar/4154691413_a695e033a8_o.jpg',
+	'https://www.youtube.com/watch?v=d1_JBMrrYw8','')
+
+movies = [toy_story, avatar]
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,32 @@
+# Lesson 3.4: Make Classes
+# Mini-Project: Movies Website
+
+# In this file, you will define the class Movie. You could do this
+# directly in entertainment_center.py but many developers keep their
+# class definitions separate from the rest of their code. This also
+# gives you practice importing Python files.
+
+import webbrowser
+from video_all import Video 
+
+
+class Movie(Video):
+    # This class provides a way to store movie related information
+    """ Movie class which takes all info about the movie and displays the trailer
+    title: string
+    movie_summary : string
+    poster_image_url : string
+    trailer_youtube_url : string
+    duration:string"""
+    def __init__(self, title, movie_summary, poster_image_url, trailer_youtube_url, duration):
+    	# initialize instance of class Movie
+    	Video.__init__(self, title, duration)
+    	self.movie_summary = movie_summary
+    	self.poster_image_url = poster_image_url
+    	self.trailer_youtube_url = trailer_youtube_url
+	"""Function to launch youtube trailer for a particular movie """
+    def show_trailer():
+    	webbrowser.open(self.trailer_youtube_url)
+
+
+
